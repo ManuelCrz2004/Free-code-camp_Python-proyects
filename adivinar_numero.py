3fb2445e28dc5d248440c7f85a9a08a0b2427de9
Manuel Cruz - Institucional <manuels.cruz@urosario.edu.co>
2022-09-08 07:07:37 -0500

    Se termina satisfactoriamente el 3er punto de progrmación en la función (computador_adivina). Se finaliza una parte del trabajo antes de ir a clase

--- adivinar_numero.py
+++ adivinar_numero.py
@@ -24,27 +24,56 @@ def usuario_adivina(x):
             contador += 1
 
 def computador_adivina(x):
-    num_bajo = 1
-    num_alto = x
+    limite_menor = 1
+    limite_mayor = x
     respuesta = ''
-    contador = 0
-    
-    while respuesta != 'c':
-        if num_bajo != num_alto:
-            numero_azar = random.randint(num_bajo, num_alto)
+    resultado = False
+    intentos = 0
+
+    while resultado == False:
+
+        numero_azar = random.randint(limite_menor, limite_mayor)
+
+        respuesta = int(input(f"> ¿Es {numero_azar} mayor (1), menor (2) o igual (0) al numero que tiene en mente?\t> "))
+
+        if respuesta == 1:
+            limite_mayor = numero_azar - 1
+            intentos += 1
+        elif respuesta == 2:
+            limite_menor = numero_azar + 1
+            intentos +=1
+        elif respuesta == 0:
+            print(f"Felicidades! La computadora lo resolvio en un total de {intentos} intentos")
+        else:
+            print("No se entendio tu petición")
+            pass
+
+
+
+    # Esta respuesta no fue usada, ya que el codigo no tiene un correcto funcionamiento
+
+    # while respuesta != 'c':
+
+    #     new_numero_azar = numero_azar
             
-        respuesta = input(f"> Es {numero_azar} muy alto [A], muy bajo [B] o correcto [C]: ")
+    #     respuesta = input(f"> Es {new_numero_azar} mayor [A], menor [B] o correcto [C]: ")
         
-        if respuesta == 'c' or 'C':
-            print(f"Excelente! Adivinaste en un total de {contador} intentos")
-        elif respuesta == 'a' or 'A':
-            num_alto = respuesta + 1
-            contador += 1
-            return num_alto
-        elif respuesta == 'b' or 'B':
-            num_bajo = respuesta - 1
-            contador += 1
-            return num_bajo
+    #     if respuesta == 'c' or 'C':
+    #         print(f"Excelente! Adivinaste en un total de {contador} intentos")
+    #     elif respuesta == 'a' or 'A':
+    #         num_alto = respuesta - 1
+    #         numero_azar = random.randint(num_bajo, num_alto)
+    #         contador += 1
+
+    #         return numero_azar
+    #     elif respuesta == 'b' or 'B':
+    #         num_bajo = respuesta + 1
+    #         numero_azar = random.randint(num_bajo, num_alto)
+    #         contador += 1
+
+    #         return numero_azar
+
+    # Esta respuesta no fue usada, ya que el codigo no tiene un correcto funcionamiento
 
 if __name__ == "__main__":
     x = int(input("> Ingrese el numero limite para adivinar (Este debe ser mayor que 1): "))
